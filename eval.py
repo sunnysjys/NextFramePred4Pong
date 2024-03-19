1208bd95c116f308d797d2903d9175c791c4cbb5
sunnysjys <sunnysjys@gmail.com>
2024-03-18 23:20:44 -0700

    EVAL script for shape and loc generalization + viz

--- eval.py
+++ eval.py
@@ -7,14 +7,15 @@ from torch.utils.data import DataLoader, ConcatDataset
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+import cv2
 from tqdm import tqdm
 
 from dataset_2 import PongDataset
 from model_just_transformer import ViViT_modified
 from utils import save_prediction_examples, log_to_file
+from utils import calculate_euclidean_distance, find_contours_and_center
 
-
-def load_model(model_path, device, N_predictions=2):
+def load_model(model_path, device, N_predictions=4):
     model = ViViT_modified(N_predictions=N_predictions).to(device)
     checkpoint = torch.load(model_path, map_location=device)
     model_state_dict = checkpoint['model_state_dict']
@@ -22,61 +23,195 @@ def load_model(model_path, device, N_predictions=2):
 
     model.eval()  # Set the model to evaluation mode
     return model
-def evaluate_model(model, dataloader, device, loss_fn, image_size, N_predictions, epoch=299, log_every=25, custom_log_path=None):
+
+def evaluate_model(model, dataloader, device, loss_fn, image_size, N_predictions, N_input_frames, model_save_path, epoch=299, log_every=25, custom_log_path=None):
     """Evaluate the model on the test set."""
     model.eval()  # Ensure the model is in evaluation mode
     losses = []
     frame_counter = 0
+    # Placeholder arrays for circularity and location distance measurements
+    shape_dissimilarity = []
+    location_distances = []
+
     with torch.no_grad():
         for batch_index, (inputs, targets) in enumerate(tqdm(dataloader, desc="Evaluating")):
             inputs, targets = inputs.to(device), targets.to(device)
             predictions = model(inputs)
-            targets_flattened = targets.reshape(-1, N_predictions, image_size**2)
-            loss = loss_fn(predictions, targets_flattened)
+            
+            predictions = predictions.view(-1, N_predictions, image_size, image_size)  # Reshape predictions
+
+            batch_contours_predictions = []
+            batch_contours_targets = []
+
+            for i in range(predictions.shape[0]):  # Iterate through batch
+                example_contours_predictions = []
+                example_contours_targets = []
+                for j in range(N_predictions):  # Iterate through each prediction
+                    pred_img = predictions[i, j].detach().cpu().numpy()
+                    # Threshold to convert predictions to binary image
+                    _, binary_pred_img = cv2.threshold(pred_img, 0.5, 1, cv2.THRESH_BINARY)
+
+                    # Find contours and center
+                    contour, center = find_contours_and_center(binary_pred_img.astype(np.uint8))
+                    example_contours_predictions.append((contour, center))
+
+                    if contour is not None:
+                        contour_t, center_t = find_contours_and_center(targets[i, :, j, :, :].reshape((32,32)).detach().cpu().numpy().astype(np.uint8)) 
+                        example_contours_targets.append((contour_t, center_t))
+                        if contour_t is not None and len(contour_t) > 0:
+                            area = cv2.contourArea(contour_t)
+                        # when the area is in the acceptable range, otherwise discard
+                        if area <= 57:
+                            shape_dissimilarity.append(cv2.matchShapes(contour_t, contour, 1, 0.0))
+                            # Calculate Euclidean distance for location distance
+                            distance = calculate_euclidean_distance(center, center_t)
+                            location_distances.append(distance)
+                        else: 
+                            print(f'{i}, {j}, DISCARDED')
+                batch_contours_predictions.append(example_contours_predictions)
+                batch_contours_targets.append(example_contours_targets)
+
+            targets_reshaped = targets.view(-1, N_predictions * image_size * image_size)  # Flatten targets for BCELoss
+            predictions_reshaped = predictions.view(-1, N_predictions * image_size * image_size)  # Flatten predictions
+            loss = loss_fn(predictions_reshaped, targets_reshaped)
             losses.append(loss.item())
             if frame_counter % log_every == 0:
-                save_prediction_examples(model_save_path, epoch, batch_index, inputs.cpu(), predictions.cpu(), targets.cpu(), image_size, N_predictions, N_input_frames, examples_to_save=2, custom_predictions_path=custom_log_path)
+                save_prediction_examples(model_save_path, epoch, batch_index, inputs.cpu(), predictions.cpu(), targets.cpu(), image_size, N_predictions, N_input_frames, 
+                examples_to_save=100, custom_predictions_path=custom_log_path, contours_predictions=batch_contours_predictions, contours_targets=batch_contours_targets)
             frame_counter += inputs.size(0)
 
+            # shape_dissimilarity.append(batch_shape_dissimilarity)
+            # location_distances.append(batch_location_distance)
+
     avg_loss = sum(losses) / len(losses)
-    return sum(losses), avg_loss
-
-
+    # print('max location distance is ', np.max(location_distances))
+    # print('max shape dissimilarity is ', np.max(shape_dissimilarity))
+    return sum(losses), avg_loss, shape_dissimilarity, location_distances
 
 if __name__ == "__main__":
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    test_id = 'test_14'
+
+    epoch = 299
+    test_id = 'test_14'     # (jack:TODO) specify run name 
     model_save_path = f'./results/{test_id}/'
-    custom_log_path = f'./eval/{test_id}/'
-    circ_test_data_path = './frames/test_12/'
-    sq_test_data_path = './frames/test_14_square/'
+    model_path = f'{model_save_path}latest_checkpoint.pth' # change to epoch number
+    custom_log_path = f'./eval/{test_id}/test_run/'
+    shape_labels = ['test_12_circle', 'test_14_square']     # (jack:TODO) change the shapes/data folder name to the one you want to test
+    # (sunny:TODO) add more models so that we can compare across models?
     image_size = 32
     batch_size = 8
     N_input_frames = 4
     N_predictions = 4
-    epoch = 299
-
-    model_path = f'{model_save_path}latest_checkpoint.pth'  # Update xx with the epoch you want to evaluate
-    dataset_s = PongDataset(directory_path=sq_test_data_path, pixel_size=image_size,
-                          N_predictions=N_predictions, N_input_frames=N_input_frames,
-                          mode='test'
-                          , rotation=True, frac_rotated=1,
-                          channel_first=True
-                          )
-    dataset_c = PongDataset(directory_path=circ_test_data_path, pixel_size=image_size,
-                          N_predictions=N_predictions, N_input_frames=N_input_frames
-                          , rotation = True, frac_rotated=1,
-                          channel_first=True
-                          )   
-    dataset = ConcatDataset([dataset_s, dataset_c]) 
-    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
-
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = load_model(model_path, device, N_predictions)
     loss_fn = torch.nn.BCEWithLogitsLoss()
 
-    total_loss, avg_loss = evaluate_model(model, dataloader, device, loss_fn, image_size, N_predictions, epoch=epoch, custom_log_path=custom_log_path)
+    avg_location_distances = []  # To store average location distances
+    max_location_distances = []  # To store max location distances
+    min_location_distances = []
+    avg_shape_dissims = []
+    max_shape_dissims = []
+    min_shape_dissims = []
+    avg_losses = []  # To store average losses
 
-    log_message = f"Evaluation completed: Total Loss: {total_loss:.4f}, Average Loss: {avg_loss:.4f}"
-    # Determine the actual log file path
-    log_file_path = os.path.join(custom_log_path, 'val_loss_log.txt') if custom_log_path else os.path.join(model_save_path, 'training_log.txt')
-    log_to_file(log_file_path, log_message)
+    for shape_label in shape_labels:
+        test_data_path = f'./frames/{shape_label}'
+    
+        print("reading data from ", test_data_path)
+        dataset = PongDataset(directory_path=test_data_path, pixel_size=image_size,
+                          N_predictions=N_predictions, N_input_frames=N_input_frames,
+                          mode='test',
+                          frac_rotated=0,
+                          channel_first=True
+                          )
+        dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=4)
+        print("saving val frames to ", custom_log_path)
+        total_loss, avg_loss, shape_dissimilarity, loc_dist = evaluate_model(model, dataloader, device, loss_fn, image_size, N_predictions, N_input_frames, model_save_path,
+                                epoch=epoch, custom_log_path=custom_log_path)
+        
+        log_message = f"Evaluation completed for shape {shape_label}: Total Loss: {total_loss:.4f}, Average Loss: {avg_loss:.4f}"
+        # Determine the actual log file path
+        log_file_path = os.path.join(custom_log_path, 'val_loss_log.txt')
+        log_to_file(log_file_path, log_message)
+
+        max_location_distances.append(np.max(loc_dist))
+        min_location_distances.append(np.min(loc_dist))
+        avg_location_distances.append(np.mean(loc_dist))
+        max_shape_dissims.append(np.max(shape_dissimilarity))
+        min_shape_dissims.append(np.min(shape_dissimilarity))
+        avg_shape_dissims.append(np.mean(shape_dissimilarity))
+        avg_losses.append(avg_loss)
+
+    print('max_location_distances', max_location_distances)
+    print('min_location_distances', min_location_distances)
+    print('avg_location_distances', avg_location_distances)
+    print('max_shape_dissims', max_shape_dissims)
+    print('min_shape_dissims', min_shape_dissims)
+    print('avg_shape_dissims', avg_shape_dissims)
+    print('avg_losses', avg_losses)
+
+    # Calculate error bars (difference from the average)
+    error_loc = [(avg - min_val, max_val - avg) for avg, min_val, max_val in zip(avg_location_distances, min_location_distances, max_location_distances)]
+    error_shape = [(avg - min_val, max_val - avg) for avg, min_val, max_val in zip(avg_shape_dissims, min_shape_dissims, max_shape_dissims)]
+    print('error_shape', error_shape)
+    print('error_loc', error_loc)
+
+    error_shape_negative = [e[0] for e in error_shape]
+    error_shape_positive = [e[1] for e in error_shape]
+    error_shape_combined = [error_shape_negative, error_shape_positive]
+    error_loc_negative = [e[0] for e in error_loc]
+    error_loc_positive = [e[1] for e in error_loc]
+    error_loc_combined = [error_loc_negative, error_loc_positive]
+
+    # Plotting the bar chart with error bars
+    x = np.arange(len(shape_labels))
+    width = 0.35
+    fig, (ax1, ax2) = plt.subplots(1, 2, figsize=(12, 6))
+
+    bars1 = ax1.bar(x - width/2, avg_shape_dissims, width, label='Average Shape Dissimilarity', color='SkyBlue', alpha=0.6)
+    bars2 = ax1.bar(x + width/2, avg_location_distances, width, label='Average Location Distances', color='IndianRed', alpha=0.6, capsize=5)
+
+    # Adding customized error bars separately
+    ax1.errorbar(x - width/2, avg_shape_dissims, yerr=error_shape_combined, fmt='none', ecolor='black', capsize=5, elinewidth=2)
+    ax1.errorbar(x + width/2, avg_location_distances, yerr=error_loc_combined, fmt='none', ecolor='black', capsize=5, elinewidth=2)
+
+    for bars, avg_vals in zip([bars1, bars2], [avg_shape_dissims, avg_location_distances]):
+        for bar, avg_val in zip(bars, avg_vals):
+            height = bar.get_height()
+            ax1.annotate(f'{avg_val:.5f}',
+                        xy=(bar.get_x() + bar.get_width() / 2, height),
+                        xytext=(0, 3),  # 3 points vertical offset
+                        textcoords="offset points",
+                        ha='center', va='bottom')
+    ax1.set_xlabel('Shape')
+    ax1.set_ylabel('Dissimilarity & Distance')
+    ax1.set_xticks(x)
+    ax1.set_xticklabels(shape_labels)
+    ax1.set_yscale('log') # (TODO) change to log scale if needed
+
+    # Plot for Average Losses
+    x_range = max(x) - min(x)
+    padding = x_range * 0.1  # Add 10% of the range as padding on each side
+    ax2.set_xlim([min(x) - padding, max(x) + padding])
+    ax2.scatter(x, avg_losses, color='DarkGreen', label='Average Loss', s=100)
+    ax2.set_yscale('log')  # (TODO) change use log scale for losses
+
+    # Annotations for the second plot
+    for i, loss in enumerate(avg_losses):
+        ax2.annotate(f'{loss:.5f}', xy=(x[i], loss), xytext=(0, 5), textcoords='offset points', ha='center', va='bottom')
+
+    ax2.set_xlabel('Shape')
+    ax2.set_ylabel('Average Loss')
+    ax2.set_xticks(x)
+    ax2.set_xticklabels(shape_labels)
+
+    ax1.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)
+    ax2.legend(loc='upper left', bbox_to_anchor=(1.05, 1), borderaxespad=0.)
+
+    fig.suptitle('Evaluation Metrics by Shape')
+
+    # Adjust layout
+    plt.tight_layout(rect=[0, 0, 1, 0.95])  # Adjust the rectangle in which the subplots fit
+    plt.subplots_adjust(right=0.85) # Adjust this value based on your legend size and preference
+
+    plt.show()
+    plt.savefig(os.path.join(custom_log_path, 'metrics_by_shape.png'))
--- utils.py
+++ utils.py
@@ -1,39 +1,92 @@
 # utils.py
 import os 
+import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 import matplotlib.patches as patches
 
-def save_prediction_examples(model_save_path, epoch, example_number, inputs, predictions, targets, image_size, N_predictions, N_input_frames, examples_to_save=5, custom_predictions_path=None):
+def save_prediction_examples(model_save_path, epoch, example_number, inputs, predictions, targets, image_size, N_predictions, N_input_frames, 
+                             examples_to_save=100, custom_predictions_path=None, 
+                             contours_predictions=None, contours_targets=None):
     test_predictions_path = custom_predictions_path if custom_predictions_path else os.path.join(model_save_path, 'test_predictions')
     os.makedirs(test_predictions_path, exist_ok=True)
 
     inputs = inputs.reshape(-1, N_input_frames, image_size, image_size)
     predictions = predictions.reshape(-1, N_predictions, image_size, image_size)
     targets = targets.reshape(-1, N_predictions, image_size, image_size)
-
     for i in range(min(examples_to_save, predictions.size(0))):
         num_columns = max(N_input_frames, N_predictions, 2)
         fig, axs = plt.subplots(3, num_columns, figsize=(num_columns * 3, 9))
 
         for n in range(num_columns):
             for row, frames in enumerate([inputs, targets, predictions]):
-                if n < len(frames[i]):
-                    axs[row, n].imshow(frames[i, n].detach().cpu().numpy(), cmap='gray')
+                if n < frames.shape[1]:  # Ensure index is within the range for current frame set
+                    img = frames[i, n].detach().cpu().numpy()
+                    axs[row, n].imshow(img, cmap='gray')
                     axs[row, n].set_title(['Input', 'Actual', 'Prediction'][row] + f' {n+1}')
                     axs[row, n].axis('off')
-                    # Adjust rectangle size to fit the image exactly
-                    rect = patches.Rectangle((-0.5, -0.5), image_size, image_size, linewidth=1, edgecolor='black', facecolor='none')
-                    axs[row, n].add_patch(rect)
-                else:
-                    axs[row, n].axis('off')
 
+                    # Check for valid contour information before attempting to draw
+                    # print('contours_predictions', contours_predictions[i])
+                    # print('n', n, 'len(contours_predictions[i])', len(contours_predictions[i]))
+                    if row == 2 and contours_predictions is not None and n < len(contours_predictions[i]):  # For Predictions
+                        contour, _ = contours_predictions[i][n]  # Extract the contour part of the tuple
+                        if contour is not None:
+                            contour = contour.squeeze()  # Ensure the contour is properly squeezed for plotting
+                            axs[row, n].plot(contour[:, 0], contour[:, 1], 'r-', linewidth=1)  # Draw contour lines in red
+                    elif row == 1 and contours_targets is not None and n < len(contours_targets[i]):  # Similar logic for Targets
+                        contour, _ = contours_targets[i][n]  # Extract the contour part of the tuple
+                        if contour is not None:
+                            contour = contour.squeeze()  # Ensure the contour is properly squeezed for plotting
+                            axs[row, n].plot(contour[:, 0], contour[:, 1], 'r-', linewidth=1)  # Draw contour lines in red
         plt.tight_layout()
-        plt.savefig(f'{test_predictions_path}/epoch_{epoch}_batch_{example_number}_example_{i}.png')
+        print(f'saving fig {test_predictions_path}epoch_{epoch}_batch_{example_number}_example_{i}.png')
+        plt.savefig(f'{test_predictions_path}epoch_{epoch}_batch_{example_number}_example_{i}.png')
         plt.close()
+
+
 
 def log_to_file(logfile, message):
     os.makedirs(os.path.dirname(logfile), exist_ok=True)
     print("logging to ", logfile)
     with open(logfile, "a") as f:
         f.write(message + "\n")
+
+
+def calculate_euclidean_distance(p1, p2):
+    return np.sqrt((p1[0] - p2[0]) ** 2 + (p1[1] - p2[1]) ** 2)
+
+def find_contours_and_center(binary_img):
+    binary_img = binary_img.copy()
+    binary_img[:3, :] = 255  # Set top 2 rows to white
+    binary_img[-3:, :] = 255  # Set bottom 2 rows to white
+    
+    # add two columns to the left AND right
+    modified_img = np.pad(binary_img, ((2, 2), (2, 2)), mode='constant', constant_values=255)
+
+    # Detect contours in the cropped image
+    contours, _ = cv2.findContours(modified_img, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # print('contours, ', contours)
+    # Sort the contours based on the contour area in descending order
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    if len(sorted_contours) > 1:
+        # The second largest contour in the cropped image
+        second_largest_contour = sorted_contours[1]
+    else:
+        print("Less than two contours found. ERROR")
+        second_largest_contour = sorted_contours[0]
+    
+    # Adjust the contour's coordinates to match their original positions
+    # by subtracting 2 from their x-location
+    second_largest_contour_adjusted = np.array([[point[0][0] - 2, point[0][1] - 2] for point in second_largest_contour])
+    
+    # Calculate moments for the adjusted contour to find its center
+    M = cv2.moments(second_largest_contour_adjusted)
+    if M["m00"] == 0:
+        return second_largest_contour_adjusted, (0, 0)
+    
+    center_x = int(M["m10"] / M["m00"])
+    center_y = int(M["m01"] / M["m00"])
+    
+    return second_largest_contour_adjusted, (center_x, center_y)
